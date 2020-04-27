3ea6d15ce6d41acbb5c6c9ed700c09d7ea7c17f1
hrtarposhyan <hrtarposhyan@gmail.com>
2020-04-27 16:46:23 +0400

    add-Migration

---
+++ Library.Api/Library.Api/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Library.Api.Entities
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; }
+
+        public Guid AuthorId { get; set; }
+    }
+}
--- Library.Api/Library.Api/Startup.cs
+++ Library.Api/Library.Api/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Api.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,8 @@ namespace Library.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("libraryDBConnectionString")), ServiceLifetime.Singleton);
             services.AddControllers();
         }
 
